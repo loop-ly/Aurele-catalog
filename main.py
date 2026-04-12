e2a007e71aefdab8401f4e8cf3f296b165f6acc5
Ibtihal butt <ibtihalbutt7@gmail.com>
2026-04-12 19:55:16 +0500

    added product count

---
+++ main.py
@@ -0,0 +1,18 @@
+a = 8
+b = 3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a%b)
+
+list = [33, 55, 66, 22]
+#print(marks)
+
+#list = ["apple", "banana", "stawberry"]
+list.insert(0,34)
+list.remove(22)
+list.pop(2)
+print(list)
+
